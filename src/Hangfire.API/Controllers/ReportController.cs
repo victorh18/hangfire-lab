f8d5ed2e537c7de4e2bfe4f005bf7535ae56bd26
Victor de la Rosa <victorwmh18@gmail.com>
2025-07-08 16:13:46 -0400

    feat: some weird stuff that kinda did not work xd

--- src/Hangfire.API/Controllers/ReportController.cs
+++ src/Hangfire.API/Controllers/ReportController.cs
@@ -32,6 +32,20 @@ namespace Hangfire.API.Controllers
             }
         }
 
+        [Route("frontend/{id}")]
+        public async Task FrontendWs(string id)
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await HandleFrontendSocket(id, webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
         private async Task HandleWebSocket(string id, WebSocket webSocket)
         {
             var buffer = new byte[1];
@@ -39,28 +53,66 @@ namespace Hangfire.API.Controllers
 
             var text = "";
 
-            while (!receiveResult.CloseStatus.HasValue)
+            while (!(webSocket.State == WebSocketState.CloseReceived))
+            {
+                // if (buffer[0] == 0)
+                // {
+                //     receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                //     continue;
+                // }
+
+                // text += Encoding.UTF8.GetString(buffer);
+                // receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                // if (receiveResult.EndOfMessage)
+                // {
+                //     text += Encoding.UTF8.GetString(buffer);
+                //     Console.WriteLine($"Message received from the {id} connection: {text}");
+                //     text = "";
+                //     buffer[0] = 0;
+                //     Console.WriteLine("message finished");
+                // }
+            }
+
+            await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, CancellationToken.None);
+            _webSocketProvider.webSockets.Remove(id);
+        }
+
+        private async Task HandleFrontendSocket(string id, WebSocket frontendWebSocket)
+        {
+            var reportingWebSocket = _webSocketProvider.webSockets.GetValueOrDefault(id);
+            var text = "";
+
+            if (reportingWebSocket == null)
+                return;
+
+            var buffer = new byte[1];
+            var bufferingResult = await reportingWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!bufferingResult.CloseStatus.HasValue)
             {
                 if (buffer[0] == 0)
                 {
-                    receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    bufferingResult = await reportingWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
                     continue;
                 }
 
                 text += Encoding.UTF8.GetString(buffer);
-                receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                bufferingResult = await reportingWebSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-                if (receiveResult.EndOfMessage)
+                if (bufferingResult.EndOfMessage)
                 {
                     text += Encoding.UTF8.GetString(buffer);
+                    var messageBytes = Encoding.UTF8.GetBytes(text);
                     Console.WriteLine($"Message received from the {id} connection: {text}");
-                    text = "";
+                    await frontendWebSocket.SendAsync(messageBytes, WebSocketMessageType.Text, true, CancellationToken.None);
                     buffer[0] = 0;
-                    Console.WriteLine("message finished");
+                    text = "";
                 }
+                
             }
-
-            await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, CancellationToken.None);
+            
+            await reportingWebSocket.CloseAsync(bufferingResult.CloseStatus.Value, bufferingResult.CloseStatusDescription, CancellationToken.None);
             _webSocketProvider.webSockets.Remove(id);
         }
     }
